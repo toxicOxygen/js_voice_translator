61bf7a4215eb791e36f5232650f7bbaa942f4f79
kwaku96 <kwakukusi30@outlook.com>
2020-10-29 15:41:03 +0300

    made massive changes

--- main.js
+++ main.js
@@ -1,69 +1,171 @@
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
-var eng_box = document.querySelector("#eng_box");
-var trans_box = document.querySelector("#trans_box");
-var langs = document.querySelectorAll('#to .nav-link');
-var current_lng = "tr";
-
-
 const voiceRecognition = new SpeechRecognition();
 
-function handleTranslationSwitch(selected){
-    langs.forEach(lang =>{
-        if (lang !== selected){
-            lang.classList.remove('active');
-        }else{
-            lang.classList.add('active');
-            current_lng = lang.dataset.lng;
+let selectedLangFrom = "English";
+let selectedLangTo = "Turkish";
+
+let langFromOptions = document.querySelectorAll("#lang-from>a");
+let langToOptions = document.querySelectorAll("#lang-to>a");
+let langIn = document.querySelector("#langIn");
+
+let swapLangBtn = document.querySelector("#btn-exchange");
+let micBtn = document.querySelector("#mic-btn");
+let clrBtn = document.querySelector("#clear-txt");
+
+let translatedTxt = document.querySelector("#translatedTxt");
+
+micBtn.addEventListener('click',(e)=>{
+    micBtn.style.color = "red";
+    voiceRecognition.start();
+});
+
+langFromOptions.forEach((tab)=>{
+    tab.addEventListener('click',(e)=>{
+        langFromOptions.forEach((item)=>{
+            if (item !== e.target)
+            {
+                item.classList.remove("active");
+            }
+            else
+            {
+                item.classList.add("active");
+                selectedLangFrom = item.textContent;
+                voiceRecognition.lang = getLang(selectedLangFrom);
+            }
+        });
+    });
+});
+
+langToOptions.forEach((tab)=>{
+    tab.addEventListener('click',(e)=>{
+        langToOptions.forEach((item)=>{
+            if (item !== e.target)
+            {
+                item.classList.remove("active");
+            }
+            else
+            {
+                item.classList.add("active");
+                selectedLangTo = item.textContent;
+            }
+        });
+    });
+});
+
+swapLangBtn.addEventListener('click',(e)=>{
+    langFromOptions.forEach((item)=>{
+        if (item.textContent !== selectedLangTo)
+        {
+            item.classList.remove("active");
+        }
+        else
+        {
+            item.classList.add("active");
         }
     });
-}
 
-function processVoice(e){
+    langToOptions.forEach((item)=>{
+        if (item.textContent !== selectedLangFrom)
+        {
+            item.classList.remove("active");
+        }
+        else
+        {
+            item.classList.add("active");
+        }
+    });
+    [selectedLangFrom,selectedLangTo] = [selectedLangTo,selectedLangFrom];
+});
+
+clrBtn.addEventListener('click',(e)=>{
+    langIn.value = "";
+    translatedTxt.textContent = "";
+    clrBtn.style.display = "none";
+});
+
+function processVoice(e)
+{
     let transcript = Array.of(e.results)
         .map(val=>val[0])
         .map(val=>val[0].transcript)
         .join('');
     
-    console.log("current lang "+current_lng);
-    writeToScreen(transcript);
-    getTranslation(transcript,current_lng);
+    langIn.value = `${transcript}`.trim();
+    if (langIn.value.length !== 0)
+    {
+        clrBtn.style.display = "block";
+    }
+    let url = buildUrl(transcript,getLang(selectedLangFrom),getLang(selectedLangTo));
+    getTranslation(url);
+}
+
+function getLang(a)
+{
+    if (a === "English")
+        return "en";
+    else if (a === "Turkish")
+        return "tr";
+    else
+        return "de";
 }
 
 function buildUrl(sentence="Hello world",from="en",to='fr'){
-    sentence.split(' ').join('%20');
-    return `https://api.mymemory.translated.net/get?q=${sentence}!&langpair=${from}|${to}`;
+    let h = sentence.split(' ').join('%20').split('\n').join('%0A');
+    return `https://api.mymemory.translated.net/get?q=${h}!&langpair=${from}|${to}`;
 }
 
-function getTranslation(sentence,to,from="en"){
-    let url = buildUrl(sentence,from,to);
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+function getTranslation(url)
+{
+    console.log(url);
+    if (selectedLangFrom === selectedLangTo)
+    {
+        translatedTxt.textContent = langIn.value;
+        return;
+    }
     fetch(url)
     .then(res=>res.json())
     .then(res=>{
-        writeToScreen(res.responseData.translatedText,false);
+        translatedTxt.textContent = res.responseData.translatedText;
     })
     .catch(err=>{
         console.log(err);
     })
 }
 
-function writeToScreen(sentence,isSpoken=true){
-    let p = document.createElement('p');
-    p.classList.add('lead');
-    p.innerText = sentence;
-    if(isSpoken){
-        eng_box.appendChild(p);
-    }else{
-        trans_box.appendChild(p);
+var checkInput = debounce(function(){
+    if (langIn.value.length !== 0)
+    {
+        clrBtn.style.display = "block";
     }
-}
+    else
+    {
+        clrBtn.style.display = "none";
+    }
+    let url = buildUrl(langIn.value,getLang(selectedLangFrom),getLang(selectedLangTo));
+    getTranslation(url);
+},3000);
 
-langs.forEach(lang => {
-    lang.addEventListener('click',(e)=>handleTranslationSwitch(lang));
-});
+langIn.addEventListener('input',checkInput);
 
 voiceRecognition.addEventListener('result',processVoice);
-voiceRecognition.addEventListener('end',voiceRecognition.start);
+voiceRecognition.addEventListener('end',(e)=>{
+    micBtn.style.color = "#828282";
+});
 
-
-voiceRecognition.start();
+// voiceRecognition.start();
